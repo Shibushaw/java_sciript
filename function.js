db896da05c5bc4ff5350168ac6aa06fce8ac4dca
Shibu <67646704+botsonshibu@users.noreply.github.com>
2021-11-09 13:29:55 +0530

    Function new method added

--- function.js
+++ function.js
@@ -1,14 +1,19 @@
+//METHOD - Q1 NORMAL FUNCTION CREATION:
 function simple(val_1){
 	console.log(val_1 + 5)
 }
 
+//calling the function
 simple(2)
 
 
 
+//ARROW FUNCTION:
 
+// function name = (function_arguments) => result of the function
 const val = (val_1,val_2) => val_1 + val_2;
 
+//calling the arrow function:
 console.log(val(10,10) + " " +  "values")
 
 
